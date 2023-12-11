e77430368431ec6b51f6c30270ee7632b7d4d09c
Infinity <200540107113@darshan.ac.in>
2023-12-11 12:20:06 +0530

    Added rewrite rule for all routes and added page not found

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import SelectAll from "./Components/SelectAll";
 import SelectByID from "./Components/SelectByID";
 import SelectAllTopic from "./Components/SelectAllTopic";
 import SelectByTopicName from "./Components/SelectByTopicName";
 import Layout from "./Components/Layout";
 import Home from "./Components/Home";
-import './CSS/Style.css'
+import "./CSS/Style.css";
+import PageNotFound from "./Components/PageNotFound";
+import { useEffect } from "react";
 
 function App() {
+  const RedirectToSelectByID = () => {
+    const { id } = useParams();
+    let navigate = useNavigate();
+    useEffect(() => {
+      navigate(`/programs/program/${id}`, { replace: true });
+    }, [navigate,id]);
+
+    return null;
+  };
+  const RedirectToSelectByTopicName = () => {
+    let { name } = useParams();
+    let navigate = useNavigate();
+    useEffect(() => {
+      navigate(`/programs/${name}`, { replace: true });
+    }, [navigate,name]);
+
+    return null;
+  };
+
   return (
     <div className="App darkTheme">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/SelectAll" element={<SelectAll />} />
-            <Route path='/SelectAll/SelectByID/:id' element={<SelectByID />} ></Route>
-            <Route path='/SelectAllTopic' element={<SelectAllTopic />} />
-            <Route path='/SelectAllTopic/SelectByTopicName/:name' element={<SelectByTopicName />} />
+            <Route path="/" element={<Home />}></Route>
+            <Route
+              path="/SelectAll"
+              element={<Navigate to="/programs" replace />}
+            />
+            <Route path="/programs" element={<SelectAll />} />
+            <Route
+              path="/programs/SelectByID/:id"
+              element={<RedirectToSelectByID />}
+            />
+            <Route
+              path="/programs/program/:id"
+              element={<SelectByID />}
+            ></Route>
+            <Route path="/SelectAllTopic" element={<SelectAllTopic />} />
+            <Route
+              path="/SelectAllTopic/SelectByTopicName/:name"
+              element={<RedirectToSelectByTopicName />}
+            />
+            <Route
+              path="/programs/:name"
+              element={<SelectByTopicName />}
+            />
+            <Route path="*" exact element={<PageNotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/Components/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div style={{height:"50vh"}} className='text-white d-flex justify-content-center align-items-center'>
+      <h1>Page Not Found</h1>
+    </div>
+  )
+}
+
+export default PageNotFound;
